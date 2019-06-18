418f1185d20c496cf4b011015ff6481118248950
Girish <41146358+grshprajapat@users.noreply.github.com>
2019-06-18 22:34:06 +0530

    Add files via upload
    array

---
+++ math.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import numpy as np 
+#creatinng 1 d array 
+a= np.array([1,5,6,9])
+b=np.array([5,6,8,3])
+ # adding same dimension array
+c=a+b
+print(c)
+m = a*b
+print(m)
+#add particular value in each element of array
+a5=a+5
+print(a5)
